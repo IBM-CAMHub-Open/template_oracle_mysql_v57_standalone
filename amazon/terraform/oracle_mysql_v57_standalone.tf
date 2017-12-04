294598530f925d6c16485b58ea042bc9bd17f1ef
CAMHub PublishBot <travis@example.org>
2017-12-04 22:44:44 +0000

    Automated CAMHub::publish on 2017-12-04 22:44:44 UTC

--- amazon/terraform/oracle_mysql_v57_standalone.tf
+++ amazon/terraform/oracle_mysql_v57_standalone.tf
@@ -11,31 +11,36 @@
 ##############################################################
 # Keys - CAMC (public/private) & optional User Key (public) 
 ##############################################################
-variable "user_public_ssh_key" {
-  type = "string"
-  description = "User defined public SSH key used to connect to the virtual machine. The format must be in openSSH."
-  default = "None"
-}
-
-variable "ibm_pm_public_ssh_key" {
-  description = "Public CAMC SSH key value which is used to connect to a guest, used on VMware only."
+variable "ibm_pm_public_ssh_key_name" {
+  description = "Public CAMC SSH key name used to connect to the virtual guest."
 }
 
 variable "ibm_pm_private_ssh_key" {
   description = "Private CAMC SSH key (base64 encoded) used to connect to the virtual guest."
 }
 
-variable "allow_unverified_ssl" {
-  description = "Communication with vsphere server with self signed certificate"
-  default = "true"
+variable "user_public_ssh_key" {
+  type = "string"
+  description = "User defined public SSH key used to connect to the virtual machine. The format must be in openSSH."
+  default = "None"
+}
+
+variable "aws_ami_owner_id" {
+  description = "The AMI Owner ID"
+  default = "309956199498"
+}
+
+variable "aws_region" {
+  description = "The aws region"
+  default = "us-east-1"
 }
 
 ##############################################################
-# Define the vsphere provider 
+# Define the aws provider 
 ##############################################################
-provider "vsphere" {
-  allow_unverified_ssl = "${var.allow_unverified_ssl}"
-  version = "~> 0.4"
+provider "aws" {
+  region = "${var.aws_region}"
+  version = "~> 1.2"
 }
 
 provider "camc" {
@@ -44,6 +49,28 @@ provider "camc" {
 
 provider "random" {
   version = "~> 1.0"
+}
+
+data "aws_vpc" "selected_vpc" {
+  filter {
+    name = "tag:Name"
+    values = ["${var.aws_vpc_name}"]
+  }
+}
+
+#Parameter : aws_vpc_name
+variable "aws_vpc_name" {
+  description = "The name of the aws vpc"
+}
+
+data "aws_security_group" "aws_sg_camc_name_selected" {
+  name = "${var.aws_sg_camc_name}"
+  vpc_id = "${data.aws_vpc.selected_vpc.id}"
+}
+
+#Parameter : aws_sg_camc_name
+variable "aws_sg_camc_name" {
+  description = "The name of the aws security group for automation content"
 }
 
 resource "random_id" "stack_id" {
@@ -59,6 +86,14 @@ variable "ibm_stack_name" {
 }
 
 #### Default OS Admin User Map ####
+variable "default_os_admin_user" {
+  type        = "map"
+  description = "look up os_admin_user using resource image"
+  default = {
+    ubuntu_images_ubuntu_xenial-16.04_099720109477 = "ubuntu"
+    RHEL-7.4_HVM_GA_309956199498                   = "ec2-user"
+  }
+}
 
 ##### Environment variables #####
 #Variable : ibm_pm_access_token
@@ -94,10 +129,34 @@ variable "ibm_sw_repo_user" {
 
 
 ##### MySQLNode01 variables #####
+#Variable : MySQLNode01-flavor
+variable "MySQLNode01-flavor" {
+  type = "string"
+  description = "MySQLNode01 Flavor"
+  default = "t2.medium"
+}
+
+data "aws_ami" "MySQLNode01_ami" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["${var.MySQLNode01-image}*"]
+  }
+  owners = ["${var.aws_ami_owner_id}"]
+}
+
 #Variable : MySQLNode01-image
 variable "MySQLNode01-image" {
   type = "string"
   description = "Operating system image id / template that should be used when creating the virtual image"
+  default = "RHEL-7.4_HVM_GA"
+}
+
+#Variable : MySQLNode01-mgmt-network-public
+variable "MySQLNode01-mgmt-network-public" {
+  type = "string"
+  description = "Expose and use public IP of virtual machine for internal communication"
+  default = "true"
 }
 
 #Variable : MySQLNode01-name
@@ -221,128 +280,78 @@ variable "MySQLNode01_mysql_version" {
   default = "5.7.17"
 }
 
+##### domain name #####
+variable "runtime_domain" {
+  description = "domain name"
+  default = "cam.ibm.com"
+}
+
 
 #########################################################
 ##### Resource : MySQLNode01
 #########################################################
 
-variable "MySQLNode01-os_password" {
+
+#Parameter : MySQLNode01_subnet_name
+data "aws_subnet" "MySQLNode01_selected_subnet" {
+  filter {
+    name = "tag:Name"
+    values = ["${var.MySQLNode01_subnet_name}"]
+  }
+}
+
+variable "MySQLNode01_subnet_name" {
   type = "string"
-  description = "Operating System Password for the Operating System User to access virtual machine"
+  description = "AWS Subnet Name"
 }
 
-variable "MySQLNode01_folder" {
-  description = "Target vSphere folder for virtual machine"
-}
 
-variable "MySQLNode01_datacenter" {
-  description = "Target vSphere datacenter for virtual machine creation"
-}
-
-variable "MySQLNode01_domain" {
-  description = "Domain Name of virtual machine"
-}
-
-variable "MySQLNode01_number_of_vcpu" {
-  description = "Number of virtual CPU for the virtual machine, which is required to be a positive Integer"
-  default = "2"
-}
-
-variable "MySQLNode01_memory" {
-  description = "Memory assigned to the virtual machine in megabytes. This value is required to be an increment of 1024"
-  default = "4096"
-}
-
-variable "MySQLNode01_cluster" {
-  description = "Target vSphere cluster to host the virtual machine"
-}
-
-variable "MySQLNode01_dns_suffixes" {
-  type = "list"
-  description = "Name resolution suffixes for the virtual network adapter"
-}
-
-variable "MySQLNode01_dns_servers" {
-  type = "list"
-  description = "DNS servers for the virtual network adapter"
-}
-
-variable "MySQLNode01_network_interface_label" {
-  description = "vSphere port group or network label for virtual machine's vNIC"
-}
-
-variable "MySQLNode01_ipv4_gateway" {
-  description = "IPv4 gateway for vNIC configuration"
-}
-
-variable "MySQLNode01_ipv4_address" {
-  description = "IPv4 address for vNIC configuration"
-}
-
-variable "MySQLNode01_ipv4_prefix_length" {
-  description = "IPv4 prefix length for vNIC configuration. The value must be a number between 8 and 32"
-}
-
-variable "MySQLNode01_adapter_type" {
-  description = "Network adapter type for vNIC Configuration"
-  default = "vmxnet3"
-}
-
-variable "MySQLNode01_root_disk_datastore" {
-  description = "Data store or storage cluster name for target virtual machine's disks"
-}
-
-variable "MySQLNode01_root_disk_type" {
+#Parameter : MySQLNode01_associate_public_ip_address
+variable "MySQLNode01_associate_public_ip_address" {
   type = "string"
-  description = "Type of template disk volume"
-  default = "eager_zeroed"
+  description = "Assign a public IP"
+  default = "true"
 }
 
-variable "MySQLNode01_root_disk_controller_type" {
+
+#Parameter : MySQLNode01_root_block_device_volume_type
+variable "MySQLNode01_root_block_device_volume_type" {
   type = "string"
-  description = "Type of template disk controller"
-  default = "scsi"
+  description = "AWS Root Block Device Volume Type"
+  default = "gp2"
 }
 
-variable "MySQLNode01_root_disk_keep_on_remove" {
+
+#Parameter : MySQLNode01_root_block_device_volume_size
+variable "MySQLNode01_root_block_device_volume_size" {
   type = "string"
-  description = "Delete template disk volume when the virtual machine is deleted"
-  default = "false"
+  description = "AWS Root Block Device Volume Size"
+  default = "25"
 }
 
-# vsphere vm
-resource "vsphere_virtual_machine" "MySQLNode01" {
-  name = "${var.MySQLNode01-name}"
-  domain = "${var.MySQLNode01_domain}"
-  folder = "${var.MySQLNode01_folder}"
-  datacenter = "${var.MySQLNode01_datacenter}"
-  vcpu = "${var.MySQLNode01_number_of_vcpu}"
-  memory = "${var.MySQLNode01_memory}"
-  cluster = "${var.MySQLNode01_cluster}"
-  dns_suffixes = "${var.MySQLNode01_dns_suffixes}"
-  dns_servers = "${var.MySQLNode01_dns_servers}"
 
-  network_interface {
-    label = "${var.MySQLNode01_network_interface_label}"
-    ipv4_gateway = "${var.MySQLNode01_ipv4_gateway}"
-    ipv4_address = "${var.MySQLNode01_ipv4_address}"
-    ipv4_prefix_length = "${var.MySQLNode01_ipv4_prefix_length}"
-    adapter_type = "${var.MySQLNode01_adapter_type}"
+#Parameter : MySQLNode01_root_block_device_delete_on_termination
+variable "MySQLNode01_root_block_device_delete_on_termination" {
+  type = "string"
+  description = "AWS Root Block Device Delete on Termination"
+  default = "true"
+}
+
+resource "aws_instance" "MySQLNode01" {
+  ami = "${data.aws_ami.MySQLNode01_ami.id}"
+  instance_type = "${var.MySQLNode01-flavor}"
+  key_name = "${var.ibm_pm_public_ssh_key_name}"
+  vpc_security_group_ids = ["${data.aws_security_group.aws_sg_camc_name_selected.id}"]
+  subnet_id = "${data.aws_subnet.MySQLNode01_selected_subnet.id}"
+  associate_public_ip_address = "${var.MySQLNode01_associate_public_ip_address}"
+  tags {
+    Name = "${var.MySQLNode01-name}"
   }
 
-  disk {
-    type = "${var.MySQLNode01_root_disk_type}"
-    template = "${var.MySQLNode01-image}"
-    datastore = "${var.MySQLNode01_root_disk_datastore}"
-    keep_on_remove = "${var.MySQLNode01_root_disk_keep_on_remove}"
-    controller_type = "${var.MySQLNode01_root_disk_controller_type}"
-  }
-
-  # Specify the connection
+  # Specify the ssh connection
   connection {
-    type = "ssh"
-    user = "${var.MySQLNode01-os_admin_user}"
-    password = "${var.MySQLNode01-os_password}"
+    user = "${var.MySQLNode01-os_admin_user == "" ? lookup(var.default_os_admin_user, format("%s_%s", replace(var.MySQLNode01-image, "/", "_"), var.aws_ami_owner_id)) : var.MySQLNode01-os_admin_user}"
+    private_key = "${base64decode(var.ibm_pm_private_ssh_key)}"
   }
 
   provisioner "file" {
@@ -357,47 +366,34 @@ resource "vsphere_virtual_machine" "MySQLNode01" {
 # =================================================================
 #!/bin/bash
 
-if (( $# != 3 )); then
-echo "usage: arg 1 is user, arg 2 is public key, arg3 is CAMC Public Key"
-exit -1
+if (( $# != 2 )); then
+    echo "usage: arg 1 is user, arg 2 is public key"
+    exit -1
 fi
 
-userid="$1"
-ssh_key="$2"
-camc_ssh_key="$3"
+userid=$1
+ssh_key=$2
+
+if [[ $ssh_key = 'None' ]]; then
+  echo "skipping add, 'None' specified"
+  exit 0
+fi
 
 user_home=$(eval echo "~$userid")
 user_auth_key_file=$user_home/.ssh/authorized_keys
-echo "$user_auth_key_file"
 if ! [ -f $user_auth_key_file ]; then
-echo "$user_auth_key_file does not exist on this system, creating."
-mkdir $user_home/.ssh
-chmod 700 $user_home/.ssh
-touch $user_home/.ssh/authorized_keys
-chmod 600 $user_home/.ssh/authorized_keys
+  echo "$user_auth_key_file does not exist on this system"
+  exit -1
 else
-echo "user_home : $user_home"
+  echo "user_home --> $user_home"
 fi
 
-if [[ $ssh_key = 'None' ]]; then
-echo "skipping user key add, 'None' specified"
-else
-echo "$user_auth_key_file"
-echo "$ssh_key" >> "$user_auth_key_file"
+echo $ssh_key >> $user_auth_key_file
 if [ $? -ne 0 ]; then
-echo "failed to add to $user_auth_key_file"
-exit -1
+  echo "failed to add to $user_auth_key_file"
+  exit -1
 else
-echo "updated $user_auth_key_file"
-fi
-fi
-
-echo "$camc_ssh_key" >> "$user_auth_key_file"
-if [ $? -ne 0 ]; then
-echo "failed to add to $user_auth_key_file"
-exit -1
-else
-echo "updated $user_auth_key_file"
+  echo "updated $user_auth_key_file"
 fi
 
 EOF
@@ -407,10 +403,28 @@ EOF
   provisioner "remote-exec" {
     inline = [
       "bash -c 'chmod +x MySQLNode01_add_ssh_key.sh'",
-      "bash -c './MySQLNode01_add_ssh_key.sh  \"${var.MySQLNode01-os_admin_user}\" \"${var.user_public_ssh_key}\" \"${var.ibm_pm_public_ssh_key}\">> MySQLNode01_add_ssh_key.log 2>&1'"
+      "bash -c './MySQLNode01_add_ssh_key.sh  \"${var.MySQLNode01-os_admin_user}\" \"${var.user_public_ssh_key}\">> MySQLNode01_add_ssh_key.log 2>&1'"
     ]
   }
 
+  root_block_device {
+    volume_type = "${var.MySQLNode01_root_block_device_volume_type}"
+    volume_size = "${var.MySQLNode01_root_block_device_volume_size}"
+    #iops = "${var.MySQLNode01_root_block_device_iops}"
+    delete_on_termination = "${var.MySQLNode01_root_block_device_delete_on_termination}"
+  }
+
+  user_data = "${data.template_cloudinit_config.MySQLNode01_init.rendered}"
+}
+data "template_cloudinit_config" "MySQLNode01_init"  {
+  part {
+    content_type = "text/cloud-config"
+    content = <<EOF
+hostname: ${var.MySQLNode01-name}
+fqdn: ${var.MySQLNode01-name}.${var.runtime_domain}
+manage_etc_hosts: false
+EOF
+  }
 }
 
 #########################################################
@@ -418,7 +432,7 @@ EOF
 #########################################################
 
 resource "camc_bootstrap" "MySQLNode01_chef_bootstrap_comp" {
-  depends_on = ["camc_vaultitem.VaultItem","vsphere_virtual_machine.MySQLNode01"]
+  depends_on = ["camc_vaultitem.VaultItem","aws_instance.MySQLNode01"]
   name = "MySQLNode01_chef_bootstrap_comp"
   camc_endpoint = "${var.ibm_pm_service}/v1/bootstrap/chef"
   access_token = "${var.ibm_pm_access_token}"
@@ -426,10 +440,10 @@ resource "camc_bootstrap" "MySQLNode01_chef_bootstrap_comp" {
   trace = true
   data = <<EOT
 {
-  "os_admin_user": "${var.MySQLNode01-os_admin_user}",
+  "os_admin_user": "${var.MySQLNode01-os_admin_user == "default"? lookup(var.default_os_admin_user, format("%s_%s", replace(var.MySQLNode01-image, "/", "_"), var.aws_ami_owner_id)) : var.MySQLNode01-os_admin_user}",
   "stack_id": "${random_id.stack_id.hex}",
   "environment_name": "_default",
-  "host_ip": "${vsphere_virtual_machine.MySQLNode01.network_interface.0.ipv4_address}",
+  "host_ip": "${var.MySQLNode01-mgmt-network-public == "false" ? aws_instance.MySQLNode01.private_ip : aws_instance.MySQLNode01.public_ip}",
   "node_name": "${var.MySQLNode01-name}",
   "node_attributes": {
     "ibm_internal": {
@@ -459,10 +473,10 @@ resource "camc_softwaredeploy" "MySQLNode01_oracle_mysql_base" {
   trace = true
   data = <<EOT
 {
-  "os_admin_user": "${var.MySQLNode01-os_admin_user}",
+  "os_admin_user": "${var.MySQLNode01-os_admin_user == "default"? lookup(var.default_os_admin_user, format("%s_%s", replace(var.MySQLNode01-image, "/", "_"), var.aws_ami_owner_id)) : var.MySQLNode01-os_admin_user}",
   "stack_id": "${random_id.stack_id.hex}",
   "environment_name": "_default",
-  "host_ip": "${vsphere_virtual_machine.MySQLNode01.network_interface.0.ipv4_address}",
+  "host_ip": "${var.MySQLNode01-mgmt-network-public == "false" ? aws_instance.MySQLNode01.private_ip : aws_instance.MySQLNode01.public_ip}",
   "node_name": "${var.MySQLNode01-name}",
   "runlist": "role[oracle_mysql_base]",
   "node_attributes": {
@@ -557,7 +571,7 @@ EOT
 }
 
 output "MySQLNode01_ip" {
-  value = "VM IP Address : ${vsphere_virtual_machine.MySQLNode01.network_interface.0.ipv4_address}"
+  value = "Private : ${aws_instance.MySQLNode01.private_ip} & Public : ${aws_instance.MySQLNode01.public_ip}"
 }
 
 output "MySQLNode01_name" {
